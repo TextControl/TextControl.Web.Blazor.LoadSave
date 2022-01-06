14f067bdd25144fd544e88d9ed7eaaeec6b828e4
Bjoern Meyer <bjoern@textcontrol.com>
2022-01-06 12:44:17 +0100

    Add project files.

---
+++ BlazorApp3/wwwroot/scripts/TextControl.js
@@ -0,0 +1,18 @@
+﻿var TextControl = TextControl || {};
+
+TextControl.saveDocument = function (dotNetObject) {
+
+	// save the document on TXTextControl object
+	TXTextControl.saveDocument(TXTextControl.StreamType.InternalUnicodeFormat, function (document) {
+		// call the .NET method 'ProcessDocument' with the saved document data
+		dotNetObject.invokeMethodAsync('ProcessDocument', document.data);
+	});
+
+};
+
+TextControl.loadDocument = function (document) {
+
+	// load the document back into the editor (TXTextControl)
+	TXTextControl.loadDocument(TXTextControl.StreamType.InternalUnicodeFormat, document);
+
+};
